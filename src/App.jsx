15387558e35c40b52f9eaec4e40f11503999fbc5
Jerico <ecofranco1@gmail.com>
2023-09-13 21:24:39 +0800

    added cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import Home from "./components/Home";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [itemId, setItemId] = useState();
+
+  const handleId = (passId) => {
+    setItemId(passId);
+  };
+
   return (
     <div className="over  flow-hidden">
       <BrowserRouter>
-        <Header />
+        <Header passId={itemId} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
+          <Route
+            path="/products"
+            element={<Products handlePassId={handleId} />}
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const Products = () => {
+const Products = (passId) => {
   const [clothes, setClothes] = useState([]);
   useEffect(() => {
     const fetchClothes = () => {
@@ -11,6 +11,10 @@ const Products = () => {
     fetchClothes();
   }, []);
 
+  const handleCart = (id) => {
+    passId.handlePassId(id);
+  };
+
   return (
     <section className="bg-[#01161e] h-full md:h-screen pt-20 text-center">
       <h1 className="font-bold text-3xl  text-[#eff6e0]">
@@ -19,17 +23,19 @@ const Products = () => {
       <div className="grid grid-cols-1 md:grid-cols-4 place-items-center gap-5 pt-10">
         {clothes.map((item) => (
           <div
-            className="border-2 w-[300px] rounded h-[350px] px-10 py-5 cursor-pointer bg-white"
+            className="border-2 max-w-[300px] xl:w-[300px] rounded h-[350px] px-10 py-5 cursor-pointer bg-white"
             key={item.id}>
             <img
               className="max-h-[160px] m-auto mb-3"
               src={item.image}
               alt={item.title}
             />
-            <h2 className="h-[50px] mb-3">{item.title}</h2>
+            <h2 className="h-[50px] xl: mb-3">{item.title}</h2>
             <p className="text-green-700 mb-2">${item.price}</p>
             <div className="flex flex-col gap-2">
-              <button className="bg-[#124559] p-2 rounded font-bold text-white">
+              <button
+                className="bg-[#124559] p-2 rounded font-bold text-white"
+                onClick={() => handleCart(item.id)}>
                 Add to cart
               </button>
             </div>
