3d61cba98aa79b6b22706ae0e1c8310d52803b1f
Jerico <116884354+cout05@users.noreply.github.com>
2023-10-14 21:00:22 +0800

    fixed the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,35 @@
 import { Link } from "react-router-dom";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SidebarContext } from "../context/SidebarContext";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { SlMagnifier } from "react-icons/sl";
 
 const Header = () => {
   const { isOpen, setIsOpen } = useContext(SidebarContext);
+
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div className="flex justify-between py-6 px-8 fixed w-screen text-white ">
+    <div
+      className="flex justify-between py-3 px-3 md:px-8 fixed
+            left-1/2 transform -translate-x-1/2  w-screen xl:w-[1000px]
+             text-[#2D3142] bg-[#fff] md:rounded-[100px] ">
       <Link to="/">
-        <h1 className="text-2xl">ApexAdorn</h1>
+        <h1 className="text-xl md:text-2xl">ApexAdorn</h1>
       </Link>
 
-      <div className="flex gap-5">
+      <div className="hidden md:flex gap-2 items-center">
+        <input
+          type="text"
+          placeholder="Search an item..."
+          className="rounded outline-none text-[#000000] p-1"
+        />
+        <SlMagnifier className="text-2xl cursor-pointer" />
+      </div>
+
+      <div className="hidden md:flex gap-5 items-center">
         <div onClick={handleClick} className="cursor-pointer">
           <AiOutlineShoppingCart className="text-2xl" />
         </div>
