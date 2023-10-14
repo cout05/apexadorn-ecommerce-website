f460a4872c257a2e02b94a1dcced0777ee778cbb
Jerico <116884354+cout05@users.noreply.github.com>
2023-10-14 20:58:59 +0800

    updated the hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import hero from "../assets/undraw_businessman_e7v0.svg";
 
 const Home = () => {
   return (
-    <section className="bg-[url('assets/wave-haikei.png')] flex flex-col justify-center items-center md:block h-screen bg-cover md:bg-center md:pt-32 text-[#eff6e0]">
-      <div className="max-w-[600px] text-center md:m-auto">
-        <h1
-          className=" text-7xl md:text-8xl
-         font-bold font-sans">
-          ApexAdorn
-        </h1>
-      </div>
-
-      <h2 className="text-2xl md:text-3xl text-center pt-10">
-        Elevated Fashion, Apex Quality
-      </h2>
-      <div className="text-center mt-16">
-        <Link to="/products" className="p-4 bg-[#1c1c22] rounded-lg">
-          Shop Now
-        </Link>
+    <section className="bg-[#D8D5DB] flex flex-col justify-center items-center md:block h-screen md:pt-32 text-[#2D3142">
+      <div className="xl:flex items-center justify-center max-w-[600px] text-center md:m-auto">
+        <img src={hero} alt="hero" className="w-[300px] m-auto" />
+        <div>
+          <h1 className="text-3xl">
+            Experience the Art of Adornment with Apex Adorn Store
+          </h1>
+          <div className="text-center mt-16">
+            <Link
+              to="/products"
+              className="p-4 bg-[#1c1c22] rounded-lg text-[#EAE8FF]">
+              Shop Now
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
