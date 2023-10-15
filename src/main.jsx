8faaa226855bdf4d555bb36aff440d3a21e28b8a
Jerico <116884354+cout05@users.noreply.github.com>
2023-10-15 18:40:41 +0800

    added providers

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,21 @@ import "./index.css";
 import ProductProvider from "./context/ProductContext.jsx";
 import SidebarProvider from "./context/SidebarContext.jsx";
 import CartItemProvider from "./context/CartItemContext.jsx";
+import SearchProvider from "./context/SearchContext.jsx";
+import MobileSidebarProvider from "./context/MobileSidebarContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <CartItemProvider>
-    <SidebarProvider>
-      <ProductProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </ProductProvider>
-    </SidebarProvider>
-  </CartItemProvider>
+  <MobileSidebarProvider>
+    <SearchProvider>
+      <CartItemProvider>
+        <SidebarProvider>
+          <ProductProvider>
+            <React.StrictMode>
+              <App />
+            </React.StrictMode>
+          </ProductProvider>
+        </SidebarProvider>
+      </CartItemProvider>
+    </SearchProvider>
+  </MobileSidebarProvider>
 );
