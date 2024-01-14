373c0bcb19e54a5f6fef4064e24e145a5f55eb41
Jerico <116884354+cout05@users.noreply.github.com>
2024-01-14 21:43:59 +0800

    removed footer temporarily

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-5 bg-[#1c1c22] flex flex-col gap-1 text-center md:text-start md:flex-row justify-center items-center md:justify-between text-white">
+    <footer className="z-10 relative p-5 bg-[#1c1c22] flex flex-col gap-1 text-center md:text-start md:flex-row justify-center items-center md:justify-between text-white">
       <div>
         <h1 className="text-3xl">Apex Adorn</h1>
       </div>
