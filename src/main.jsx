081806f4372bb698cefdce6df5218a32128b1bac
Jerico <116884354+cout05@users.noreply.github.com>
2024-01-14 21:43:24 +0800

    remove unused context

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,15 @@ import "./index.css";
 import ProductProvider from "./context/ProductContext.jsx";
 import SidebarProvider from "./context/SidebarContext.jsx";
 import CartItemProvider from "./context/CartItemContext.jsx";
-import SearchProvider from "./context/SearchContext.jsx";
-import MobileSidebarProvider from "./context/MobileSidebarContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <MobileSidebarProvider>
-    <SearchProvider>
-      <CartItemProvider>
-        <SidebarProvider>
-          <ProductProvider>
-            <React.StrictMode>
-              <App />
-            </React.StrictMode>
-          </ProductProvider>
-        </SidebarProvider>
-      </CartItemProvider>
-    </SearchProvider>
-  </MobileSidebarProvider>
+  <CartItemProvider>
+    <SidebarProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </SidebarProvider>
+  </CartItemProvider>
 );
