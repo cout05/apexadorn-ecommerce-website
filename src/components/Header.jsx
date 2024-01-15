aeaea98dd1cf841f3c1ceba3f291a8a733121640
Jerico <116884354+cout05@users.noreply.github.com>
2024-01-15 21:24:32 +0800

    adjusted navbar for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-[#eff6e0] md:fixed md:w-[20%] h-10 md:h-screen border">
-      <div className="bg-[#1c1c22] flex items-center justify-between text-[#eff6e0] px-2  md:p-0 md:ml-4 md:mr-5 md:text-center md:rounded-md md:my-4 h-[50px] md:h-[150px]">
+      <div className="bg-[#1c1c22] flex relative items-center justify-between text-[#eff6e0] px-2  md:p-0 md:ml-4 md:mr-5 md:text-center md:rounded-md md:my-4 h-[50px] md:h-[150px]">
         <h1 className="text-xl md:text-2xl md:absolute md:bottom-3 w-full ">
           ApexAdorn
         </h1>
