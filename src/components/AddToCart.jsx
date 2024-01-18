727d3a65348d33423d983d5b6cda563cecc0c439
Jerico <116884354+cout05@users.noreply.github.com>
2024-01-18 18:27:21 +0800

    updated the design of modal

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -20,9 +20,9 @@ const AddToCart = (filteredProduct) => {
         className="fixed inset-0 w-full h-full bg-black/50 cursor-pointer"></div>
 
       <div className="relative w-full cursor-pointer pointer-events-none transition my-auto p-4">
-        <div className="bg-[#1c1c22] w-full py-2 cursor-default pointer-events-auto relative rounded-xl mx-auto max-w-sm">
+        <div className="bg-[#eff6e0] text-[#1c1c22] w-full py-2 cursor-default pointer-events-auto relative rounded-xl mx-auto max-w-sm">
           <div className="space-y-2 p-2">
-            <div className="p-4 space-y-2 text-center text-white">
+            <div className="p-4 space-y-2 text-center">
               <h2 className="text-xl font-bold tracking-tight">
                 Add this item to cart?
               </h2>
@@ -30,7 +30,7 @@ const AddToCart = (filteredProduct) => {
           </div>
 
           <div className="space-y-2">
-            <div aria-hidden="true" className="border-t px-2"></div>
+            <div aria-hidden="true" className=" border-t px-2"></div>
 
             <div className="px-6 py-2">
               <div className="grid gap-2 grid-cols-[repeat(auto-fit,minmax(0,1fr))]">
@@ -46,8 +46,8 @@ const AddToCart = (filteredProduct) => {
                 <button
                   type="submit"
                   onClick={add}
-                  className="inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors outline-none min-h-[2.25rem] px-4 text-sm  shadow 
-                   border-transparent bg-[#eff6e0] ">
+                  className="inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border outline-none min-h-[2.25rem] px-4 text-sm  shadow 
+                    bg-[#1c1c22] text-[#eff6e0]">
                   <span className="flex items-center gap-1">
                     <span className="">Add-To-Cart</span>
                   </span>
