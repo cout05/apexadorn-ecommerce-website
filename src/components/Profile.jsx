4d7329c1e8bfd2e10832eeb4c7293275f73eb1a8
Jerico <116884354+cout05@users.noreply.github.com>
2024-01-28 12:23:50 +0800

    changed design

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,25 +7,25 @@ import { IoIosArrowForward } from "react-icons/io";
 
 const Profile = () => {
   return (
-    <div className="md:w-[80%] md:relative flex pt-[7.3rem] md:pt-0 md:ml-auto h-screen text-[#fff]">
-      <div className="flex flex-col items-center gap-2 p-2 h-36 absolute top-[160px] md:top-[50px] left-0 right-0 md:right-[900px]">
-        <div className="w-[100px] h-[100px] bg-slate-500 rounded-full"></div>
-        <p className="text-1xl">Profile</p>
+    <div className="md:w-[80%] font-semibold md:relative flex flex-col md:flex-row pt-[7rem] md:pt-0 md:ml-auto h-screen text-[#2D3142]">
+      <div className="bg-[#fff] w-full md:w-[300px] flex flex-col md:ml-8 md:mt-4 p-4 items-center">
+        <div className="w-[100px] h-[100px] bg-[#2D3142] rounded-full"></div>
+        <p className="text-1xl font-semibold">Profile</p>
       </div>
-      <div className="bg-[#1c1c22] pt-16 md:pt-28 mt-[100px] flex-1 rounded-t-[50px] md:rounded-tl-none">
+      <div className="bg-[#fff] w-full md:w-[700px] pb-20 mt-0 md:mt-40 ml-0 md:mx-auto">
         <div className="flex gap-4 px-4 py-2 border-b border-t items-center justify-center mt-[50px] mb-16">
-          <div className="text-[#fff] text-center cursor-pointer md:flex-1">
+          <div className="text-[#2D3142] text-center cursor-pointer md:flex-1">
             <FaClipboardList className="text-2xl mx-auto" />
             Orders
           </div>
-          <div className="text-[#fff] text-center cursor-pointer md:flex-1">
+          <div className="text-[#2D3142] text-center cursor-pointer md:flex-1">
             <MdMessage className="text-2xl mx-auto" />
             Reviews
           </div>
-          <div className="text-[#fff] text-center cursor-pointer md:flex-1">
+          <div className="text-[#2D3142] text-center cursor-pointer md:flex-1">
             <RiCoupon2Fill className="text-2xl mx-auto" /> Coupons
           </div>
-          <div className="text-[#fff] text-center cursor-pointer md:flex-1">
+          <div className="text-[#2D3142] text-center cursor-pointer md:flex-1">
             <HiMiniBanknotes className="text-2xl mx-auto" />
             Payment Method
           </div>
@@ -35,7 +35,7 @@ const Profile = () => {
             personal info <IoIosArrowForward className="inline" />
           </p>
           <p>
-            address info <IoIosArrowForward className="inline" />{" "}
+            address info <IoIosArrowForward className="inline" />
           </p>
           <p>
             settings <IoIosArrowForward className="inline" />
